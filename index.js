7c42456453a5e82a5a59a5fa1c7119a763f650ca
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-09-08 23:58:04 +0200

    Added source and tests

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Check if the provided file type should be accepted by the input with accept attribute.
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Input#attr-accept
+ *
+ * Borrowed from https://github.com/enyo/dropzone
+ *
+ * @param file {Object}
+ * @param acceptedFiles {string}
+ * @returns {boolean}
+ */
+
+export default function(file, acceptedFiles) {
+    if (acceptedFiles) {
+        const acceptedFilesArray = acceptedFiles.split(',');
+        const mimeType = file.type;
+        const baseMimeType = mimeType.replace(/\/.*$/, '');
+
+        return acceptedFilesArray.some(type => {
+            const validType = type.trim();
+            if (validType.charAt(0) === '.') {
+                return (file.name.toLowerCase().indexOf(validType.toLowerCase(), file.name.length - validType.length) !== -1);
+            } else if (/\/\*$/.test(validType)) {
+                // This is something like a image/* mime type
+                return baseMimeType === validType.replace(/\/.*$/, '');
+            }
+            return mimeType === validType;
+        });
+    }
+    return true;
+}
---
+++ test/index.js
@@ -0,0 +1,104 @@
+/* eslint no-unused-expressions: 0 */
+
+import expect from 'expect';
+import accept from '../index';
+
+describe('accept', () => {
+    it('should return true if called without acceptedFiles', () => {
+        expect(accept({
+            type: 'some/type'
+        }, undefined)).toBe(true);
+    });
+
+    it('should properly validate if called with concrete mime types', () => {
+        const acceptedMimeTypes = 'text/html,image/jpeg,application/json';
+        expect(accept({
+            type: 'text/html'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'image/jpeg'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'application/json'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'image/bmp'
+        }, acceptedMimeTypes)).toBe(false);
+    });
+
+    it('should properly validate if called with base mime types', () => {
+        const acceptedMimeTypes = 'text/*,image/*,application/*';
+        expect(accept({
+            type: 'text/html'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'image/jpeg'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'application/json'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'image/bmp'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'some/type'
+        }, acceptedMimeTypes)).toBe(false);
+    });
+
+    it('should properly validate if called with mixed mime types', () => {
+        const acceptedMimeTypes = 'text/*,image/jpeg,application/*';
+        expect(accept({
+            type: 'text/html'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'image/jpeg'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'image/bmp'
+        }, acceptedMimeTypes)).toBe(false);
+        expect(accept({
+            type: 'application/json'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'some/type'
+        }, acceptedMimeTypes)).toBe(false);
+    });
+
+    it('should properly validate even with spaces in between', () => {
+        const acceptedMimeTypes = 'text/html ,   image/jpeg, application/json';
+        expect(accept({
+            type: 'text/html'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            type: 'image/jpeg'
+        }, acceptedMimeTypes)).toBe(true);
+    });
+
+    it('should properly validate extensions', () => {
+        const acceptedMimeTypes = 'text/html ,    image/jpeg, .pdf  ,.png';
+        expect(accept({
+            name: 'somxsfsd',
+            type: 'text/html'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            name: 'somesdfsdf',
+            type: 'image/jpeg'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            name: 'somesdfadfadf',
+            type: 'application/json'
+        }, acceptedMimeTypes)).toBe(false);
+        expect(accept({
+            name: 'some-file file.pdf',
+            type: 'random/type'
+        }, acceptedMimeTypes)).toBe(true);
+        expect(accept({
+            name: 'some-file.pdf file.gif',
+            type: 'random/type'
+        }, acceptedMimeTypes)).toBe(false);
+        expect(accept({
+            name: 'some-file file.png',
+            type: 'random/type'
+        }, acceptedMimeTypes)).toBe(true);
+    });
+});
