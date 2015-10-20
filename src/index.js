7fa4ae2fb0dd9b32e4cc355f304ddce4dd302495
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-10-20 22:39:03 +0200

    Use String.endsWith for a cleaner code. Include polyfills for Array.some and String.endsWith.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
  * Check if the provided file type should be accepted by the input with accept attribute.
  * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Input#attr-accept
  *
- * Borrowed from https://github.com/enyo/dropzone
+ * Inspired by https://github.com/enyo/dropzone
  *
  * @param file {File} https://developer.mozilla.org/en-US/docs/Web/API/File
  * @param acceptedFiles {string}
  * @returns {boolean}
  */
+
+import 'core-js/fn/array/some';
+import 'core-js/fn/string/ends-with';
 
 export default function(file, acceptedFiles) {
     if (file && acceptedFiles) {
@@ -19,7 +22,7 @@ export default function(file, acceptedFiles) {
         return acceptedFilesArray.some(type => {
             const validType = type.trim();
             if (validType.charAt(0) === '.') {
-                return (fileName.toLowerCase().indexOf(validType.toLowerCase(), fileName.length - validType.length) !== -1);
+                return fileName.toLowerCase().endsWith(validType.toLowerCase());
             } else if (/\/\*$/.test(validType)) {
                 // This is something like a image/* mime type
                 return baseMimeType === validType.replace(/\/.*$/, '');
