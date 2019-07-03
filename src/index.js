2cec91bb67fa64b94fe384e9593963fe3e46ce12
FUJI Goro (gfx) <gfuji@cpan.org>
2019-07-03 10:56:11 +0900

    drop core-js from dependencies which the app should be responsible for

--- src/index.js
+++ src/index.js
@@ -9,9 +9,6 @@
  * @returns {boolean}
  */
 
-import 'core-js/fn/array/some'
-import 'core-js/fn/string/ends-with'
-
 export default function(file, acceptedFiles) {
   if (file && acceptedFiles) {
     const acceptedFilesArray = Array.isArray(acceptedFiles)
